dbb53f6339c5a5727b2c8530f62e47e1389a69f7
Micah Wood <micah@wpscholar.com>
2017-02-18 13:12:30 -0600

    Created a custom page template.

---
+++ templates/welcome-to-our-blog.php
@@ -0,0 +1,15 @@
+<?php // Template Name: Welcome to Our Blog
+
+get_header(); ?>
+
+	<div class="wrap">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+
+				<h1><?php the_title(); ?></h1>
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- .wrap -->
+
+<?php get_footer(); ?>
