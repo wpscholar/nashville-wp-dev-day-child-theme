23e42add61147a86ba40cca9bbd622296e237342
Micah Wood <micah@wpscholar.com>
2017-02-18 13:55:04 -0600

    Register and display a custom nav menu.

--- functions.php
+++ functions.php
@@ -6,3 +6,9 @@ function tsc_wp_enqueue_scripts() {
 	wp_enqueue_style( 'twentyseventeen-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'twentyseventeen-child-style', get_stylesheet_directory_uri() . '/style.css' );
 }
+
+add_action( 'after_setup_theme', 'tsc_after_setup_theme' );
+
+function tsc_after_setup_theme() {
+	register_nav_menu( 'evergreen-content', __( 'Evergreen Menu', 'twentyseventeen-child' ) );
+}
--- templates/welcome-to-our-blog.php
+++ templates/welcome-to-our-blog.php
@@ -2,14 +2,23 @@
 
 get_header(); ?>
 
-	<div class="wrap">
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
+    <div class="wrap">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main" role="main">
 
-				<h1><?php the_title(); ?></h1>
+                <h1><?php the_title(); ?></h1>
 
-			</main><!-- #main -->
-		</div><!-- #primary -->
-	</div><!-- .wrap -->
+                <p>Check out these awesome posts:</p>
+
+                <?php
+				wp_nav_menu( array(
+					'theme_location' => 'evergreen-content',
+					'menu_class'     => 'evergreen-content',
+				) );
+				?>
+
+            </main><!-- #main -->
+        </div><!-- #primary -->
+    </div><!-- .wrap -->
 
 <?php get_footer(); ?>
