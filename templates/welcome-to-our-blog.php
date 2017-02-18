e4e154c7d9b1e7755588d01078d1735e8a5825fc
Micah Wood <micah@wpscholar.com>
2017-02-18 14:17:55 -0600

    Add a normal WordPress loop and custom WP_Query loop to our page template.

--- templates/welcome-to-our-blog.php
+++ templates/welcome-to-our-blog.php
@@ -8,14 +8,46 @@ get_header(); ?>
 
                 <h1><?php the_title(); ?></h1>
 
+				<?php if ( have_posts() ): ?>
+					<?php while ( have_posts() ): ?>
+						<?php the_post(); ?>
+						<?php the_content(); ?>
+					<?php endwhile; ?>
+				<?php endif; ?>
+
+                <hr/>
+
                 <p>Check out these awesome posts:</p>
 
-                <?php
+				<?php
 				wp_nav_menu( array(
 					'theme_location' => 'evergreen-content',
 					'menu_class'     => 'evergreen-content',
 				) );
 				?>
+
+                <hr/>
+
+				<?php
+				$query = new WP_Query( array(
+					'post_type'      => 'post',
+					'posts_per_page' => 5,
+				) );
+				?>
+				<?php if ( $query->have_posts() ): ?>
+                    <p>Check out our latest posts:</p>
+                    <ul>
+						<?php while ( $query->have_posts() ): ?>
+							<?php $query->the_post(); ?>
+                            <li>
+                                <a href="<?php the_permalink(); ?>">
+									<?php the_title(); ?>
+                                </a>
+                            </li>
+						<?php endwhile; ?>
+						<?php wp_reset_postdata(); ?>
+                    </ul>
+				<?php endif; ?>
 
             </main><!-- #main -->
         </div><!-- #primary -->
